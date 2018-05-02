7c9bb2ea5752bba6e1c94483f884c8a17769be6d
TAC <tac@tac42.net>
2018-05-03 01:00:12 +0900

    Add Linear Regressions

--- 2_SupervisedLearning.py
+++ 2_SupervisedLearning.py
@@ -11,15 +11,17 @@ X.shape
 y.shape
 X
 # 散布図をプロット
-mg.discrete_scatter(X[:, 0], X[:, 1], y ) #
+mg.discrete_scatter(X[:, 0], X[:, 1], y )
 plt.legend(["Class 0", "Class 1"], loc=4)
 plt.xlabel("First feature")
 plt.ylabel("Second feature")
 
 # mg.discrete_scatterを使わずにやろうと思うと・・・
+plt.scatter(X[:, 0], X[:, 1], c=y, marker='o')
+plt.legend(["Class 0", "Class 1"], loc=4)
 plt.xlabel("First feature")
 plt.ylabel("Second feature")
-plt.scatter(X[:, 0], X[:, 1], c=y, marker='o')
+
 
 # -----------------------------------------------------------
 # 回帰のサンプルデータ(waveデータセット)
@@ -27,7 +29,7 @@ X, y = mg.datasets.make_wave(n_samples=128)
 X.shape
 y.shape
 
-plt.plot(X, y, 'o')
+plt.plot(X, y, 'o' )
 plt.xlabel("Feature")
 plt.ylabel("Target")
 
@@ -43,7 +45,11 @@ cancer.keys()
 cancer.data.shape #(569,30)
 cancer.target.shape # (569,)
 # np.bincount(cancer.target) # 値ごとに集計したndarrayを返す
-{n: c for n,c in zip(cancer.target_names, np.bincount(cancer.target))} #良性/悪性のカウント
+
+cancer.feature_names
+
+ #良性/悪性のカウント
+{n: c for n,c in zip(cancer.target_names, np.bincount(cancer.target))}
 
 # さらに特徴量を交互作用(interaction)で拡張してみる
 #
@@ -53,7 +59,8 @@ cancer.target.shape # (569,)
 # forgeのデータセットに対して k-最近傍法でクラス分類してみる
 X, y = mg.datasets.make_forge()
 
-mg.plots.plot_knn_classification(n_neighbors=3) # 3近傍の例
+# 3近傍の例
+mg.plots.plot_knn_classification(n_neighbors=3)
 
 # 実際にscikit-learnでやってみる．
 X, y = mg.datasets.make_forge()
@@ -73,6 +80,28 @@ clf.predict([[10, 5]]) # 1
 
 clf.score(X_test, y_test)
 
+# これをプロットすると
+X_p = np.array([[10, 1], [10, 5]])
+y_p = clf.predict(X_p)
+plt.scatter(X_train[:, 0], X_train[:, 1], c=y_train, marker='o')
+plt.scatter(X_p[:, 0], X_p[:, 1], c=y_p, s=80, marker='*')
+
+# 決定境界を表示してみる
+X, y = mg.datasets.make_forge()
+fig, axis = plt.subplots(1, 4, figsize = (10,2))
+neighbors = [1,3,9, len(X)]
+for n, ax in zip(neighbors, axis):
+    clf = KNeighborsClassifier(n_neighbors=n).fit(X,y)
+    mg.plots.plot_2d_separator(clf, X, fill=True, eps=0.5, ax=ax, alpha=0.3)
+    mg.discrete_scatter(X[:,0], X[:,1], y, ax=ax)
+    ax.set_title("{} neighbors".format(n))
+    ax.set_xlabel("feature 0")
+    ax.set_ylabel("feature 1")
+axis[0].legend(loc=3)
+
+# 近傍点が少ないほどモデルは複雑で，多いほど単純になる
+# 近傍点を全てのテストデータポイント(len(X[:])=26)にすると，
+# 全ての点について最も多いクラスのになる．
 
 # -----------------------------------------------------------
 # cancerのデータセットに対して k-最近傍法でクラス分類してみる
@@ -82,10 +111,15 @@ X, y = cancer.data, cancer.target
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-# X_train.shape
-# X_test.shape
+X_train.shape
+X_test.shape
 
-# n=1から10を試して，精度を比較する
+plt.scatter(X_train[:, 0], X_train[:, 1], c=y_train, marker='o')
+
+cancer_df = pd.DataFrame(X_train, columns=cancer.feature_names)
+grr = pd.scatter_matrix(cancer_df, c=y_train, figsize=(16,16), hist_kwds={'bins':40}, s=60, alpha=.8)
+
+# n=1から100を試して，精度を比較する
 r = range(1,100)
 training_accuracy = []
 test_accuracy     = []
@@ -118,6 +152,8 @@ plt.xlabel("Feature")
 plt.ylabel("Target")
 
 from sklearn.neighbors.regression import KNeighborsRegressor
+X, y = mg.datasets.make_wave(n_samples=1024)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 reg = KNeighborsRegressor(n_neighbors=3).fit(X_train, y_train)
 reg.score(X_test, y_test) # 回帰問題なので R^2スコアを返す
 
@@ -125,17 +161,25 @@ reg.score(X_test, y_test) # 回帰問題なので R^2スコアを返す
 # どうやら，sinカーブを線形にバイアスしたものにランダム加算したものっぽい．
 
 
+
 # knnのnを変えながら評価してみる
+X, y = mg.datasets.make_wave(n_samples=1024)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 
 # plt.subplots(行数, 列数, figsize=(5,10))
 fig, axis = plt.subplots(5, 1, figsize=(8,16))
 
+# ついでにスコアの推移も見る
+training_accuracy = []
+test_accuracy     = []
 # np.reshape(newshape)
 # Gives a new shape to an array without changing its data.
 # reshape(n,m) => n行 m列にする．-1を指定すると要素数を元によしなにやってくれる
 line = np.linspace(-3, 3, 1000).reshape(-1, 1) # 1000行1列
 for n, ax in zip([1,3,9,15,30], axis): # axは描画先
     reg = KNeighborsRegressor(n_neighbors=n).fit(X_train, y_train)
+    training_accuracy.append(reg.score(X_train, y_train))
+    test_accuracy.append(reg.score(X_test, y_test))
     ax.plot(line, reg.predict(line)) # -3 から 3 までのデータを評価
     ax.plot(X_train, y_train, '^', c=mg.cm2(0), markersize=2)
     ax.plot(X_test,  y_test,  'v', c=mg.cm2(1), markersize=2)
@@ -157,6 +201,12 @@ axis[0].legend(["Model predictions", "Training data/target", "Test data/target"]
 # nを30まで増やしてみたが，n=9でtest_score=0.72でサチってるっぽい．
 # グラフを見ると，nが増えるほどモデルはなめらかな正弦波に近づくが，
 # ランダム変動を吸収できないので，これくらいの精度に落ち着いてしまうのでは．
+
+plt.ylabel("Accuracy") # 精度
+plt.xlabel("n")
+plt.plot([1,3,9,15,30], training_accuracy, label="training_accuracy")
+plt.plot([1,3,9,15,30], test_accuracy, label="test_accuracy")
+plt.legend() # plotで設定したlabelを表示する
 
 # -----------------------------------------------------------
 # 線形モデル
@@ -235,8 +285,12 @@ mg.plots.plot_ridge_n_samples()
 # 線形回帰ではテストデータが少ないとほとんどなにも学習できない
 # 十分な訓練データがある場合は，正則化はあまり重要ではない．
 
+
+
 # Lasso
-# リッジ回帰ではWが正則化されるので過剰適合を防げる
+X, y = mg.datasets.load_extended_boston()
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
 from sklearn.linear_model import Lasso
 lasso = Lasso().fit(X_train, y_train)
 lasso.score(X_train, y_train) # 0.29
@@ -244,6 +298,72 @@ lasso.score(X_test, y_test) # 0.20
 np.sum(lasso.coef_ == 0) # 100 の特徴量が捨てられ
 np.sum(lasso.coef_ != 0) # 4 つの特徴量だけが使われる
 
+plt.plot(lasso.coef_, "o", label = "Lasso")
+plt.xlabel("Coefficient index")
+plt.ylabel("Coefficient magnitude")
+plt.hlines(0, 0, len(lasso.coef_))
+plt.legend()
 
 
-# --------------------------
+
+# ----------------------------------------------------
+# 線形クラス分類
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm          import LinearSVC
+X, y = mg.datasets.make_forge()
+
+fig, axis = plt.subplots(4,2,figsize=(10,14))
+for c, axi in zip([0.01, 1.0, 10.0, 100.0], axis):
+    for model, ax in zip([LogisticRegression, LinearSVC], axi):
+        clf = model(C=c).fit(X,y)
+        mg.plots.plot_2d_separator(clf, X, fill=False, eps=0.5, ax=ax, alpha=.7)
+        mg.discrete_scatter(X[:, 0], X[:, 1], y, ax=ax)
+        ax.set_title("{} C={}, s={}".format(clf.__class__.__name__, c, clf.score(X,y)))
+        ax.set_xlabel("Feature 0")
+        ax.set_ylabel("Feature 1")
+        ax.legend()
+
+# 高次元データ(cancer)だとどうか・・
+from sklearn.datasets import load_breast_cancer
+cancer = load_breast_cancer()
+X, y = cancer.data, cancer.target
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+
+c = 0.100
+LogisticRegression(C=c).fit(X_train, y_train).score(X_test, y_test)
+LogisticRegression(C=c).fit(X_train, y_train).score(X_train, y_train)
+LinearSVC(C=c).fit(X_train, y_train).score(X_test, y_test)
+LinearSVC(C=c).fit(X_train, y_train).score(X_train, y_train)
+
+
+# ----------------------------------------------------
+# 多クラス分類
+from sklearn.datasets import make_blobs
+X,y = make_blobs(random_state=40)
+plt.scatter(X[:,0], X[:,1], c=y)
+# 2次元3クラス
+
+# LinearSVCで分類してみる
+from sklearn.svm import LinearSVC
+linear_svc = LinearSVC(C=10).fit(X,y)
+print("X shape: ", X.shape) # 100,2
+print("Coefficient shape: ", linear_svc.coef_.shape) #係数配列 3,2
+print("Intercept shape: ", linear_svc.intercept_.shape) # 切片配列 3,
+
+# 分類結果を見てみる
+# plt.scatter(X[:,0], X[:,1], c=y)
+mg.plots.plot_2d_classification(linear_svc, X, fill=True, alpha=.7)
+mg.discrete_scatter(X[:, 0], X[:, 1], y)
+line = np.linspace(-15, 15, 2)
+for coef, intercept, color in zip(linear_svc.coef_, linear_svc.intercept_, ['b', 'r', 'g']):
+    plt.plot(line, -(line * coef[0] + intercept) / coef[1], c=color)
+# plt.ylim(-10, 15)
+# plt.xlim(-10, 8)
+plt.xlabel("Feature 0")
+plt.ylabel("Feature 1")
+plt.legend(['Class 0', 'Class 1', 'Class 2', 'Line class 0', 'Line class 1', 'Line class 2'], loc=(1.01, 0.3))
+
+
+# EOB
